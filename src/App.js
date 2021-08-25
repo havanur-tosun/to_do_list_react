bb58423213431c1f2a30ea33726fa71306cfe156
Havanur <havanurtosun0618@gmail.comhavanurtosun0618@gmail.com>
2021-08-25 15:58:22 -0400

    finish

--- src/App.js
+++ src/App.js
@@ -1,18 +1,56 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import TodoList from "./TodoList";
+import {v4 as uuidv4} from 'uuid';
+import "./App.css";
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
-  const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
+  const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function toggleTodo(id) {
+    const newTodos = [...todos];
+    const todo = newTodos.find(todo => todo.id === id);
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value;
+    if (name === '') return
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
+  }
+
+  function handleClearTodos() {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
   return (
-    <>
-      < TodoList todos={todos}/>
-      < input type="text" />
-      <button>Add Todo</button>
-      <button>Clear Complete</button>
-      <div>0 left to do</div>
-    </>
+    <div className='container'>
+      < TodoList todos={todos} toggleTodo={toggleTodo}/>
+      < input ref={todoNameRef} type="text" id='inputField'/>
+      <button onClick={handleAddTodo} id='addToDo'>Add Todo</button>
+      <button onClick={handleClearTodos} id='clear'>Clear Complete</button>
+      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
+    </div>
     
   )
 }
 
 export default App;
+
+
+// const [todos, setTodos] = useState([{ id: 1, name: 'Todo 1', complete: false}])
